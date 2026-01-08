b7fefc6717b6bb7815460b6618322bb94ca4407c
Sibasundar Jena <sibasundarj8@gmail.com>
2026-01-09 00:23:59 +0530

    Hello

--- src/Day_04_LC_42.java
+++ src/Day_04_LC_42.java
@@ -9,7 +9,8 @@ import java.util.Scanner;
  *   Q. Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much
  *      water it can trap after raining.
  *    <br> Ex.
- *    <br>   Input : height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+ *    <br>   Input : height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]<br>
+ *    <img src="https://assets.leetcode.com/uploads/2018/10/22/rainwatertrap.png" alt="not found">
  *    <br>   Output: 6
  *    <br>   Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
  *    <br>                In this case, 6 units of rain water (blue section) are being trapped.
